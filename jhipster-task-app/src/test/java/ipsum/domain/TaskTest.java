2d324445d1c167e604efbe7b45bba55eb42bcad9
Hostilian <77862924+Hostilian@users.noreply.github.com>
2024-08-21 18:48:42 +0200

    í

---
+++ jhipster-task-app/src/test/java/ipsum/domain/TaskTest.java
@@ -0,0 +1,24 @@
+package com.dv.ipsum.domain;
+
+import static com.dv.ipsum.domain.TaskTestSamples.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.dv.ipsum.web.rest.TestUtil;
+import org.junit.jupiter.api.Test;
+
+class TaskTest {
+
+    @Test
+    void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Task.class);
+        Task task1 = getTaskSample1();
+        Task task2 = new Task();
+        assertThat(task1).isNotEqualTo(task2);
+
+        task2.setId(task1.getId());
+        assertThat(task1).isEqualTo(task2);
+
+        task2 = getTaskSample2();
+        assertThat(task1).isNotEqualTo(task2);
+    }
+}
